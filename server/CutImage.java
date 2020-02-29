83c7b5b389213d8d3926f0e4bf480454407b1ce0
totomdotcom <tom.georgelin@gmail.com>
2020-02-29 18:49:21 +0100

    Recreation et enregistrement de l'image une fois flip

--- server/CutImage.java
+++ server/CutImage.java
@@ -1,9 +1,13 @@
 import java.rmi.registry.Registry;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.RemoteException;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
 import java.rmi.NotBoundException;
 import java.util.*;
 
+import java.awt.Graphics;
 import javax.swing.ImageIcon;
 import java.io.*;
 import javax.imageio.ImageIO;
@@ -37,8 +41,31 @@ public class CutImage implements ServiceCutImage {
 		for (int i = 0 ; i < this.bandeList.size() ; i++) {
 			this.flipBandes.add(this.clientList.get(i%this.clientList.size()).traitement(this.bandeList.get(i)));
 			if (i == this.bandeList.size()-1) {
-				//créer image;
-				System.out.println("imageCréée");
+				BufferedImage result = new BufferedImage(this.bufferedImage.getWidth(), this.bufferedImage.getHeight(), BufferedImage.TYPE_INT_RGB);
+    			Graphics g = result.getGraphics();
+    			int x = 0, y = 0;
+    			for(Bande image : this.flipBandes){
+
+					Image imgI = image.getImage().getImage();
+					BufferedImage imgB = new BufferedImage(imgI.getWidth(null), imgI.getHeight(null),BufferedImage.TYPE_INT_ARGB);
+					/*Graphics2D bGr = imgB.createGraphics();
+					bGr.drawImage(imgI, 0, 0, null);
+					bGr.dispose();*/
+
+			        g.drawImage(imgB, x, y, null);
+			        x += 20;
+			        if(x > result.getWidth()){
+			            x = 0;
+			            y += imgB.getHeight();
+			        }
+    			}
+    			try {
+    				ImageIO.write(result,"png",new File("result.png"));
+    			} 
+    			catch (Exception e) {
+    				e.printStackTrace();
+    			}
+    			System.out.println("imageCréée");
 			}
 
 		}
