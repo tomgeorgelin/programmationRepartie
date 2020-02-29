f6fd24d1f5742f20dcfbcc65aa75184d4fb55ef5
totomdotcom <tom.georgelin@gmail.com>
2020-02-29 16:07:15 +0100

    Prise en compte changement bufferedimage

--- client/ReverseImage.java
+++ client/ReverseImage.java
@@ -1,6 +1,7 @@
 import java.awt.Image;
 import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
+import javax.swing.ImageIcon;
 import java.awt.image.BufferedImage;
 
 import java.io.*;
@@ -23,13 +24,14 @@ class ReverseImage implements ServiceReverseImage {
 	public Bande traitement(Bande img) {
 		if (!this.bandeList.contains(img)) {
 			AffineTransform tx = AffineTransform.getScaleInstance(-1, 1);
-	        tx.translate(-img.getImage().getWidth(null), 0);
+	        tx.translate(-img.getImage().getImage().getWidth(null), 0);
 	        AffineTransformOp op = new AffineTransformOp(tx,
 	        AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
-	        BufferedImage bimg = new BufferedImage(img.getImage().getWidth(null), img.getImage().getHeight(null), BufferedImage.TYPE_INT_RGB);
+	        BufferedImage bimg = new BufferedImage(img.getImage().getImage().getWidth(null), img.getImage().getImage().getHeight(null), BufferedImage.TYPE_INT_RGB);
 
-	        bimg = op.filter((BufferedImage)img.getImage(), null);
-	        return new Bande(img.getNb(),bimg);
+	        bimg = op.filter((BufferedImage) img.getImage().getImage(), null);
+	        System.out.println("flip : " + img.getNb());
+	        return new Bande(img.getNb(),new ImageIcon(bimg));
 		}
 		else {
 			 return null;	
