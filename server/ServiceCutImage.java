03e7fa94b8e23e1750a704f869f55b981a57879c
totomdotcom <tom.georgelin@gmail.com>
2020-03-01 14:53:13 +0100

    Changement interface service cut image

--- server/ServiceCutImage.java
+++ server/ServiceCutImage.java
@@ -2,15 +2,18 @@ import java.rmi.registry.Registry;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.RemoteException;
 import java.rmi.NotBoundException;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
 import java.awt.image.BufferedImage;
-import java.io.*;
-import javax.imageio.ImageIO;
+import java.rmi.Remote;
+import javax.swing.ImageIcon;
+import java.awt.Image;
+import java.rmi.RemoteException;
 
 interface ServiceCutImage extends Remote {
 	public void enregistrerClient(ServiceReverseImage client) throws RemoteException;
 
 	public void cutImages() throws RemoteException;
+
+	public void initialisation(ImageIcon file) throws RemoteException;
+
+	public ImageIcon lancerDistribution() throws RemoteException;
 }
