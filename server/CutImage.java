9535bc4fe504068909297c5baf6a00497fba240e
totomdotcom <tom.georgelin@gmail.com>
2020-03-01 14:55:07 +0100

    Corrections problèmes d'écoupe, correction répartition des bandes entre les clients, changememnts initialisation, mise à jour affichage serveur

--- server/CutImage.java
+++ server/CutImage.java
@@ -8,6 +8,7 @@ import java.rmi.NotBoundException;
 import java.util.*;
 
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import javax.swing.ImageIcon;
 import java.io.*;
 import javax.imageio.ImageIO;
@@ -21,72 +22,85 @@ public class CutImage implements ServiceCutImage {
 	private ArrayList<Bande> flipBandes;
 
 	private BufferedImage bufferedImage;
+	private int height;
 
-	public CutImage(String file) throws RemoteException {
+	public CutImage() throws RemoteException {
 		this.clientList = new ArrayList<>();
 		this.bandeList = new ArrayList<>();
 		this.flipBandes = new ArrayList<>();
-		try{
-			this.bufferedImage = ImageIO.read(new File(file));
-		}
-		catch(Exception e){
-			e.printStackTrace();
-		}
+		
+	}
+
+	public void initialisation(ImageIcon img) throws RemoteException {
+		this.bandeList = new ArrayList<>();
+		this.flipBandes = new ArrayList<>();
+		Image imgI = img.getImage();
+		this.bufferedImage = new BufferedImage(imgI.getWidth(null), imgI.getHeight(null),BufferedImage.TYPE_INT_RGB);
+		Graphics2D bGr = this.bufferedImage.createGraphics();
+    	bGr.drawImage(imgI, 0, 0, null);
+    	bGr.dispose();
+    	/*try {
+    		ImageIO.write(this.bufferedImage,"jpg",new File("test.jpg"));
+
+    	} catch (Exception e) {
+    		
+    	}*/
+		//this.bufferedImage = (BufferedImage)file.getImage();
 		cutImages();
 	}
 
 	public void enregistrerClient(ServiceReverseImage c) throws RemoteException {
 		this.clientList.add(c);
-		System.out.println("client ajouté");
+		System.out.println("Un nouveau client s est connecte");
+		
+	}
+
+	public ImageIcon lancerDistribution() throws RemoteException{
+		BufferedImage result = new BufferedImage(this.bufferedImage.getWidth(), this.bufferedImage.getHeight(), BufferedImage.TYPE_INT_RGB);
 		for (int i = 0 ; i < this.bandeList.size() ; i++) {
 			this.flipBandes.add(this.clientList.get(i%this.clientList.size()).traitement(this.bandeList.get(i)));
+			System.out.println("Bande numero : " + i + " donnee au client numero : " + i%this.clientList.size());
 			if (i == this.bandeList.size()-1) {
-				BufferedImage result = new BufferedImage(this.bufferedImage.getWidth(), this.bufferedImage.getHeight(), BufferedImage.TYPE_INT_RGB);
-    			Graphics g = result.getGraphics();
+    			Graphics2D g = (Graphics2D)result.getGraphics();
     			int x = 0, y = 0;
     			for(Bande image : this.flipBandes){
-
 					Image imgI = image.getImage().getImage();
-					BufferedImage imgB = new BufferedImage(imgI.getWidth(null), imgI.getHeight(null),BufferedImage.TYPE_INT_ARGB);
-					/*Graphics2D bGr = imgB.createGraphics();
+					BufferedImage imgB = new BufferedImage(imgI.getWidth(null), imgI.getHeight(null),BufferedImage.TYPE_INT_RGB);
+					
+					Graphics2D bGr = imgB.createGraphics();
 					bGr.drawImage(imgI, 0, 0, null);
-					bGr.dispose();*/
-
-			        g.drawImage(imgB, x, y, null);
-			        x += 20;
-			        if(x > result.getWidth()){
-			            x = 0;
-			            y += imgB.getHeight();
-			        }
+					bGr.dispose();
+			        g.drawImage(imgB, 0, y, null);
+					y+= imgB.getHeight();
     			}
-    			try {
-    				ImageIO.write(result,"png",new File("result.png"));
-    			} 
-    			catch (Exception e) {
-    				e.printStackTrace();
-    			}
-    			System.out.println("imageCréée");
+    			System.out.println("L image a bien ete creee");
 			}
-
 		}
+    	return new ImageIcon(result);
 	}
 
 	public void cutImages() throws RemoteException {
-		int height, nbB;
+		int nbB;
 
-		if ((Math.ceil(this.bufferedImage.getHeight()/20))%20 == 0) {
-			height = (int)Math.ceil(this.bufferedImage.getHeight()/20)+1;
+		if ((Math.ceil((double)this.bufferedImage.getHeight()/20))%20 == 0) {
+			this.height = (int)Math.ceil((double)this.bufferedImage.getHeight()/20)+1;
 			nbB = 20;
 		} else {
-			height = (int)Math.ceil(this.bufferedImage.getHeight()/20);
+			this.height = (int)Math.ceil((double)this.bufferedImage.getHeight()/20);
 			nbB = 21;
 		}
+
+
 		try {
-			for (int y = 0; y < nbB ; y++) {
-				if (this.bufferedImage.getHeight() - height*y < height) {
-				  height = (this.bufferedImage.getHeight() - height*y);
+			int c= 0;
+			for (int y = 0; y < nbB+1 ; y++) {
+				c= this.height*y;
+				if (this.bufferedImage.getHeight() - this.height*y < this.height) {
+				  this.height = (this.bufferedImage.getHeight() - this.height*y);
+
 				}
-				this.bandeList.add(new Bande(y,new ImageIcon(this.bufferedImage.getSubimage(0,height,this.bufferedImage.getWidth(), height))));
+				this.bandeList.add(new Bande(y,new ImageIcon(this.bufferedImage.getSubimage(0,c,this.bufferedImage.getWidth(), this.height))));
+			
 			}
 		}
 		catch(Exception e) {
